2149466b7e1ace5f2e3b82a6b733ff7a2edff125
Anuj <anuj.shah@simformsolutions.com>
2026-03-13 13:15:25 +0530

    Added .env for Credentials

--- Python/APIs/task2.py
+++ Python/APIs/task2.py
@@ -1,14 +1,27 @@
 import psycopg2
 import requests
+from dotenv import load_dotenv
+import os
 
 # Database connection
+load_dotenv()
+
+# read variables
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+# connect to database
 conn = psycopg2.connect(
-    host="localhost",
-    dbname="api_database",
-    user="postgres",
-    password="2004",
-    port=5432
+    host=DB_HOST,
+    port=DB_PORT,
+    dbname=DB_NAME,
+    user=DB_USER,
+    password=DB_PASSWORD
 )
+
 cur = conn.cursor()
 
 
